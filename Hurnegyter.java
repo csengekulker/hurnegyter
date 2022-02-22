76ce4971e79c5dd53d97b2be18412c12e5062410
csengekulker <k71608883429@ktch.hu>
2022-02-22 08:32:57 +0100

    program prints data and reads input

--- Hurnegyter.java
+++ Hurnegyter.java
@@ -13,5 +13,16 @@ import java.util.Scanner;
 class Hurnegyter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("\tBalogh Csenge\n\tSzoft_I_N\n\t2022.02.22");
+
+        System.out.print("A oldal > ");
+        double a = sc.nextDouble();
+        System.out.print("B oldal > ");
+        double b = sc.nextDouble();
+        System.out.print("C oldal > ");
+        double c = sc.nextDouble();
+        System.out.print("D oldal > ");
+        double d = sc.nextDouble();
     }
 }
