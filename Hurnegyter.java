774cb3a965742335ab4496b950d97efcd7cba443
csengekulker <k71608883429@ktch.hu>
2022-02-22 08:26:01 +0100

    added header comment

---
+++ Hurnegyter.java
@@ -0,0 +1,9 @@
+/*
+* File: Hurnegyter.java
+* Author: Balogh Csenge
+* Copyright: 2022, Balogh Csenge
+* Group: Szoft_I_N
+* Date: 2022_02_22
+* Github: https://github.com/csengekulker/
+* Licenc: GNU GPL
+*/
